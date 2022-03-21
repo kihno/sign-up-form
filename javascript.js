7ecaaa7f6d1a50063777230820b8fe5dbc0cd255
kihno <stistyle13@gmail.com>
2022-03-21 11:47:30 -0700

    tweak password validation

--- javascript.js
+++ javascript.js
@@ -3,8 +3,13 @@
 const passwordOne = document.getElementById('password');
 const passwordTwo = document.getElementById('confirm-password');
 const error = document.getElementById('error');
+const submit = document.getElementById('submit');
 
 passwordTwo.addEventListener('input', function(e) {
+    passValidate();
+});
+
+function passValidate() {
     if (passwordTwo.value === passwordOne.value) {
         error.textContent = '';
         passwordOne.classList.remove('invalid');
@@ -13,5 +18,17 @@ passwordTwo.addEventListener('input', function(e) {
         error.textContent = "*Passwords do not match"
         passwordOne.className = 'invalid';
         passwordTwo.className = 'invalid';
+        submit.preventDefault();
+        form.validity = false;
+    }
+};
+
+const form = document.getElementById('sign-up');
+
+form.addEventListener('submit', (e) => {
+    if(passwordTwo.className == 'invalid' || passwordTwo == '') {
+        e.preventDefault();
+    } else {
+        alert("42");
     }
 });
